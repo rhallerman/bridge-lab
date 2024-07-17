b09708ae0c46153fd7aeef61ac69e2611fdb5b88
Raphael Hallerman <raphael.hallerman@gmail.com>
2024-07-17 01:53:05 -0400

    removed logging

--- src/App.js
+++ src/App.js
@@ -118,8 +118,6 @@ function App() {
   };
 
   useEffect(() => {
-    console.log(window.location.href);
-    console.log(window.location.href.length);
     // if (window.location.href.length === 22) {
     if (window.location.href.length === 38) {
       oauthSignIn();
